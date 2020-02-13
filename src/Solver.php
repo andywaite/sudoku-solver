3711b0d0d8f089fbd2e6adbd548f609c87e0233b
Andrew Waite <andrewwaite@silktide.com>
2020-02-13 23:00:24 +0000

    Comments

--- src/Solver.php
+++ src/Solver.php
@@ -20,23 +20,42 @@ class Solver
      */
     public function solve(Grid $grid): bool
     {
+        // Loop cols
         for ($x = 0; $x < 9; $x++) {
+
+            // Loop rows
             for ($y = 0; $y < 9; $y++) {
+
+                // Check for empty cells
                 if ($grid->isEmpty($x, $y)) {
+
+                    // Loop through possible values
                     for ($try = 1; $try <= 9; $try++) {
+
+                        // If valid move (i.e. no collision)
                         if ($this->cellChecker->isValidMove($grid, $x, $y, $try)) {
+
+                            // Set value
                             $grid->setValue($x, $y, $try);
+
+                            // Recursively solve
                             if ($this->solve($grid)) {
+                                // Yay, solved!
                                 return true;
                             }
+
+                            // Must have failed, backtrack for this cell and try next
                             $grid->nullValue($x, $y);
                         }
                     }
+
+                    // This didn't work, try another route
                     return false;
                 }
             }
         }
 
+        // All complete
         return true;
     }
 }

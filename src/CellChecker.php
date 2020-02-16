08476c6cf6e2dd3dc593b3269e43521e6f9e9dfa
Andrew Waite <andrewwaite@silktide.com>
2020-02-16 19:32:03 +0000

    Make valid move checking more efficient

--- src/CellChecker.php
+++ src/CellChecker.php
@@ -112,32 +112,9 @@ class CellChecker
      */
     public function isValidMove(Grid $grid, int $x, int $y, $value): bool
     {
-        // Check for same value in X
-        for ($i = 0; $i < 9; $i++) {
-            if ($grid->getValue($x, $i) == $value) {
+        foreach ($this->linkedCells[$x][$y] as $linkedCell) {
+            if ($grid->getValue($linkedCell['x'], $linkedCell['y']) == $value) {
                 return false;
-            }
-        }
-
-        // Check for same value in Y
-        for ($i = 0; $i < 9; $i++) {
-            if ($grid->getValue($i, $y) == $value) {
-                return false;
-            }
-        }
-
-        // Check for same value in segment
-        $segmentXMin = $x - ($x%3);
-        $segmentXMax = $segmentXMin + 2;
-
-        $segmentYMin = $y - ($y%3);
-        $segmentYMax = $segmentYMin + 2;
-
-        for ($i = $segmentXMin; $i <= $segmentXMax; $i++) {
-            for ($n = $segmentYMin; $n <= $segmentYMax; $n++) {
-                if ($grid->getValue($i, $n) == $value) {
-                    return false;
-                }
             }
         }
 

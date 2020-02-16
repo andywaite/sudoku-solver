a6aa1347238a324b5550b598ba8ee238debb752f
Andrew Waite <andrewwaite@silktide.com>
2020-02-16 19:00:10 +0000

    Add benchmark script to measure fastest solution

---
+++ benchmark.md
@@ -0,0 +1,6 @@
+#Benchmarks for solvers
+| |Basic Recursive|Optimised Recursive|
+|---|---|---|
+|examples/hard.txt|0.005841|0.006439|
+|examples/harder.txt|0.250750|0.036351|
+|examples/hardest.txt|22.973750|8.346840|
---
+++ benchmark.php
@@ -0,0 +1,61 @@
+<?php
+
+include 'vendor/autoload.php';
+
+$examples = [
+    'examples/hard.txt',
+    'examples/harder.txt',
+    'examples/hardest.txt'
+];
+
+// Create our classes
+$printer = new \Andywaite\Sudoku\GridPrinter();
+$gridLoader = new \Andywaite\Sudoku\GridLoader();
+$cellChecker = new \Andywaite\Sudoku\CellChecker();
+
+$solvers = [
+    "Basic Recursive" => new \Andywaite\Sudoku\BasicRecursiveSolver($cellChecker),
+    "Optimised Recursive" => new \Andywaite\Sudoku\RecursiveSolverWithOptimisation($cellChecker)
+];
+
+$results = [];
+
+foreach ($examples as $example) {
+
+    foreach ($solvers as $title => $solver) {
+        // Load start grid from file
+        $grid = new \Andywaite\Sudoku\Grid();
+        $gridLoader->loadGrid($grid, $example);
+
+        // Solve!
+        $start = microtime(true);
+        try {
+            if (!$solver->solve($grid)) {
+                throw new Exception("Solver returned false");
+            }
+            $end = microtime(true);
+            $runTime = ($end - $start);
+        } catch (Exception $e) {
+            $runTime = "FAIL";
+        }
+
+        $results[$example][$title] =number_format($runTime, 6);
+    }
+}
+
+$benchmarkOutput = "#Benchmarks for solvers";
+$benchmarkOutput .= "\n| |";
+$blankLine = "\n|---|";
+
+foreach ($solvers as $title => $solver) {
+    $benchmarkOutput .= $title."|";
+    $blankLine .= "---|";
+}
+
+$benchmarkOutput .= $blankLine;
+
+foreach($results as $exampleFilename => $result) {
+    $benchmarkOutput .= "\n|".$exampleFilename."|".implode("|", $result)."|";
+}
+
+file_put_contents("benchmark.md", $benchmarkOutput);
